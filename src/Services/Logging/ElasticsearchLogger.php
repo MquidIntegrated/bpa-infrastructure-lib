781f3db63b8367192e6d7c59563b3de50b7e64de
Henry Onuorah <henryemekaonuorah92@gmail.com>
2024-11-17 11:29:03 +0100

    fix:middleware-error

--- src/Services/Logging/ElasticsearchLogger.php
+++ src/Services/Logging/ElasticsearchLogger.php
@@ -2,26 +2,29 @@
 
 namespace BPA\InfrastructureLib\Services\Logging;
 
+use Elasticsearch\ClientBuilder;
 use Monolog\Handler\ElasticsearchHandler;
 use Monolog\Logger;
-use Elasticsearch\ClientBuilder;
 
 class ElasticsearchLogger
 {
     public function __invoke(array $config)
     {
-        // Create an Elasticsearch client
         $client = ClientBuilder::create()
-            ->setHosts([sprintf('%s:%s', $config['host'], $config['port'])])
+            ->setHosts([
+                [
+                    'host' => $config['host'],
+                    'port' => $config['port'],
+                    'scheme' => 'http',
+                ]
+            ])
             ->build();
 
-        // Create a handler for Elasticsearch
         $handler = new ElasticsearchHandler($client, [
             'index' => $config['index'],
-            'type' => '_doc', // Type is deprecated in newer Elasticsearch versions
+            'type' => '_doc',
         ]);
 
-        // Create the logger
         return new Logger('elasticsearch', [$handler]);
     }
 }
